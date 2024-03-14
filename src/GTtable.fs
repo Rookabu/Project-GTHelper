3c4243006ba6ef0489e46641eaacdbc809e44b44
Rookabu <annika-paul1@gmx.de>
2024-03-14 13:53:20 +0100

    daisy download

--- src/GTtable.fs
+++ src/GTtable.fs
@@ -9,30 +9,11 @@ type GTtable =
     /// </summary>
     [<ReactComponent>]
     static member Main() =
-        let (count, setCount) = React.useState(0)
         Html.div [
-            Html.h1 count
             Html.button [
-                prop.onClick (fun _ -> setCount(count + 1))
-                prop.text "Increment"
+                prop.className "button"
+                prop.onClick (fun _ ->())
+                prop.text "upload abstract"
             ]    
             ]
         
-
-
-    // <summary>
-    // A React component that uses Feliz.Router
-    // to determine what to show based on the current URL
-    // </summary>
-    // [<ReactComponent>]
-    // static member Router() =
-    //     let (currentUrl, updateUrl) = React.useState(Router.currentUrl())
-    //     React.router [
-    //         router.onUrlChanged updateUrl
-    //         router.children [
-    //             match currentUrl with
-    //             | [ ] -> Html.h1 "Index"
-    //             | [ "counter" ] -> Components.Counter()
-    //             | otherwise -> Html.h1 "Not found"
-    //         ]
-    //     ]
