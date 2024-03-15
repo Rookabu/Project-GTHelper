564544bd34e2008ec65ac14570d54247e2af33c8
Rookabu <annika-paul1@gmx.de>
2024-03-15 11:55:43 +0100

    table build

--- src/GTtable.fs
+++ src/GTtable.fs
@@ -12,32 +12,52 @@ type GTtable =
     [<ReactComponent>]
     static member Main() =
         Html.div [
+            Html.h1 [
+                prop.text ""
+            ]
+            Html.div [
+                prop.text "By using GroundTruth Helper you can create a ground truth using biology abstracts to 
+                    find protein/gene partners and their type of interaction. You can also use this website to create other types of datasets, 
+                    for example to train a large language model or just to simplify your research. You can then download your created data. 
+                    Just click on each partner in the abstract after expanding the abstract to assign it to partner 1 or 2."
+            ]
             Daisy.button.button [
-                button.lg
+                button.md
                 prop.className "button"
                 //prop.className 
                 prop.onClick (fun _ ->())
                 prop.text "Upload abstract"
+                prop.fontSize 20
             ]
-            Html.div [
-                prop.text "By using GroundTruth Helper you can create a ground truth using biology abstracts to find protein/gene partners and their type of interaction. You can also use this website to create other types of datasets, for example to train a large language model or just to simplify your research. You can then download your created data.Just click on each partner in the abstract after expanding the abstract to assign it to partner 1 or 2."
-            ]
+            
             Daisy.button.button [
-                button.lg
+                button.md
                 prop.className "button"
                 //prop.className 
                 prop.onClick (fun _ ->())
                 prop.text "Download table"
             ]
-            Daisy.table [
-                Html.thead [                   
-                    Html.tr [prop.className "text"; prop.children [Html.th "Nr."; Html.th "Title"; Html.th "Partner 1"; Html.th "Partner 2"; Html.th "Interactiontype"]
+            prop.children [
+                Daisy.table [
+                    Html.thead [                   
+                        Html.tr [prop.className "text"; prop.children [Html.th "Nr."; Html.th "Title"; Html.th "Partner 1"; Html.th "Partner 2"; Html.th "Interactiontype"]
+                        ]
+                    ]
+                    Html.tbody [                    
+                        Html.tr [
+                            prop.className "hover:bg-orange-300" 
+                            prop.children [ 
+                            Html.td "1" 
+                            Html.td "Example: Reduced Dormancy5 encodes a protein phosphatase 2C that is required for seed dormancy in Arabidopsis"                       
+                            Html.td "RDO5"
+                            Html.td "APUM9"
+                            Html.td "Protein-Gene"
+                            ]
+                        ]
                     ]
                 ]
-                Html.tbody [
-                    Html.tr [ Html.td "1"; prop.children [ prop.className "hover:bg-orange-500"; Html.td "Example: Reduced Dormancy5 encodes a protein phosphatase 2C that is required for seed dormancy in Arabidopsis"]; Html.td "RDO5"; Html.td "APUM9"; Html.td "Protein-Gene"]]
-                ]
             ]
+        ]
                    
                 
         
