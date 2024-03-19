b5de5da7002b44059bfe611a77c2187d8b2d109c
Rookabu <annika-paul1@gmx.de>
2024-03-19 17:15:17 +0100

    fixed styling

--- src/GTtable.fs
+++ src/GTtable.fs
@@ -4,6 +4,134 @@ namespace Components
 open Feliz
 open Feliz.DaisyUI
 
+
+module private Helper =
+
+    let splitTextIntoWords (text: string) =
+        text.Split([|' '; '\n'; '\t'; '\r'|], System.StringSplitOptions.RemoveEmptyEntries)
+        |> Array.toList
+
+    let headerRow = 
+        Html.thead [                   
+            Html.tr [
+                prop.className "title"
+                prop.style [
+                    style.fontSize 15
+                ]
+                prop.children [
+                    Html.th "Nr."
+                    Html.th "Title"
+                    // Html.th "Partner 1" 
+                    // Html.th "Partner 2" 
+                    // Html.th "Interactiontype"
+                    Html.th ""
+                ]
+            ]
+        ]
+
+    let tableCellPaperContent (abst: string) =
+        Html.td [
+            Daisy.collapse [
+                prop.tabIndex 0
+                collapse.arrow
+                prop.children [
+                    Html.input [prop.type' "checkbox"]
+                    Daisy.collapseTitle [ 
+                        prop.style [
+                            style.fontSize 15 
+                        ]  
+                        prop.text "Example: Reduced Dormancy5 encodes a protein phosphatase 2C that is required for seed dormancy in Arabidopsis"
+                    ]
+                    Daisy.collapseContent [
+                        Daisy.cardBody [
+                            prop.style [
+                                style.flexDirection.row
+                                style.flexWrap.wrap
+                                style.fontSize 15
+                            ]    
+                            prop.children [
+                                for word in (splitTextIntoWords abst) do
+                                    Html.span [
+                                        //prop.className 
+                                        prop.onClick (fun _ ->())
+                                        prop.text word
+                                        prop.className "hover:bg-sky-700"  
+                                        prop.style [style.cursor.pointer; style.userSelect.none] 
+                                    ]
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ]
+
+    let form(inp: string, setInput) = 
+        Html.div [
+            prop.className "flex gap-1 flex-col lg:flex-row"
+            prop.children [
+                Daisy.formControl [
+                    Daisy.label [prop.className "title"; prop.text "Partner 1"; prop.style [style.fontSize 15]]
+                    Daisy.input [input.bordered; input.sm; prop.style [style.color.black; style.maxWidth 150]]
+                ]
+                Daisy.formControl [
+                    Daisy.label [prop.className "title"; prop.text"Partner 2"; prop.style [style.fontSize 15]]
+                    Daisy.input [input.bordered; input.sm; prop.style [style.color.black; style.maxWidth 150]]
+                ]
+                Daisy.formControl [
+                    Daisy.label [prop.className "title"; prop.text"InteractionType"; prop.style [style.fontSize 15]]
+                    Daisy.dropdown [
+                        Daisy.button.button [
+                            button.sm
+                            prop.style [
+                                style.width 135
+                                style.fontSize 15  
+                            ]                                            
+                            prop.text inp
+                            prop.className "tableElement"
+                        ]
+                        Daisy.dropdownContent [
+                            prop.className "p-1 shadow menu sm-base-150"
+                            prop.style [
+                                style.width 140
+                                style.fontSize 15
+                                style.zIndex 2
+                            ]
+                            prop.tabIndex 0
+                            prop.children [
+                                Html.li [Html.a [prop.text "Protein-Gene"; prop.onClick (fun _ -> setInput "Protein-Gene"); prop.className "dropDownElement"]]
+                                Html.li [Html.a [prop.text "Protein-Protein"; prop.onClick (fun _ -> setInput "Protein-Protein"); prop.className "dropDownElement"]]
+                                Html.li [Html.a [prop.text "Other"; prop.onClick (fun _ -> setInput "Other"); prop.className "dropDownElement"]]
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ]
+
+    let tableCellFormInput (inp: string, setInput) =
+        Html.td [
+            prop.className "flex"
+            prop.children [
+                Daisy.collapse [
+                    prop.style [style.overflow.visible]
+                    prop.tabIndex 0
+                    collapse.arrow
+                    prop.children [
+                        Html.input [prop.type' "checkbox"]
+                        Daisy.collapseTitle [ 
+                            prop.style [
+                                style.fontSize 15
+                            ]  
+                            prop.text "Interactions"
+                        ]
+                        Daisy.collapseContent [
+                            form(inp, setInput)
+                        ]
+                    ]
+                ]
+            ]
+        ]
+
 type GTtable =
 
     /// <summary>
@@ -12,9 +140,7 @@ type GTtable =
     [<ReactComponent>]
     static member Main() =
         let (myInput, setinput) = React.useState("Proteine-Gene")
-        let splitTextIntoWords (text: string) =
-            text.Split([|' '; '\n'; '\t'; '\r'|], System.StringSplitOptions.RemoveEmptyEntries)
-            |> Array.toList
+
 
         let exAbstract =
             "Seed dormancy determines germination timing and contributes to crop production and the adaptation of natural populations to their environment. 
@@ -28,22 +154,15 @@ type GTtable =
         Html.div [
             prop.className "childstyle"
             prop.children [
-                Html.h1 [
-                    prop.text "What is GroundTruth Helper about?"  
-                    prop.style [
-                        style.fontSize 20
-                        style.fontWeight.bold
-                        style.textDecorationLine.underline
-                    ] 
-                ]
                 Daisy.card [
                     prop.style [
                         style.fontSize 15
-                        style.width 700
+                        style.maxWidth 700
                         style.textAlign.justify
                     ] 
                     prop.children [
                         Daisy.cardBody [
+                            Daisy.cardTitle "What is GroundTruth Helper about?"
                             Html.p "By using GroundTruth Helper you can create a ground truth using biology abstracts to 
                                 find protein/gene partners and their type of interaction. You can also use this website to create other types of datasets, 
                                 for example to train a large language model or just to simplify your research. You can then download your created data. 
@@ -51,153 +170,51 @@ type GTtable =
                         ]
                     ]
                 ]
-                Daisy.button.button [
-                    button.md
-                    prop.className "button"
-                    prop.onClick (fun _ ->())
-                    prop.text "Upload abstract"
-                ]
-                
-                Daisy.button.button [
-                    button.md
-                    prop.className "button"
-                    //prop.className 
-                    prop.onClick (fun _ ->())
-                    prop.text "Download table"
+                Html.div [
+                  prop.className "flex size-full justify-between"
+                  prop.style [
+                    style.paddingLeft 100
+                    style.paddingRight 100
+                  ]
+                  prop.children [
+                    Daisy.button.button [
+                        button.md
+                        prop.className "button"
+                        prop.onClick (fun _ ->())
+                        prop.text "Upload abstract"
+                    ]
+                    
+                    Daisy.button.button [
+                        button.md
+                        prop.className "button"
+                        //prop.className 
+                        prop.onClick (fun _ ->())
+                        prop.text "Download table"
+                    ]
+                  ]
                 ]
                 Daisy.table [
-                    table.zebra
                     prop.style [
                         style.fontSize 16
-                        style.width 1500
+                        style.maxWidth 1500
                     ]
                     prop.children [
-                        Html.thead [                   
-                            Html.tr [
-                                prop.className "title"
-                                prop.style [
-                                    style.fontSize 15
-                                ]
-                                prop.children [
-                                    Html.th "Nr."
-                                    Html.th "Title"
-                                    // Html.th "Partner 1" 
-                                    // Html.th "Partner 2" 
-                                    // Html.th "Interactiontype"
-                                    Html.th ""
-                                ]
-                            ]
-                        ]
+                        Helper.headerRow
                         Html.tbody [
                             //         Html.td "RDO5"
                             //         Html.td "APUM9"
                             Html.tr [
-                                // prop.style [style.width 900]
                                 prop.children [
                                     Html.td "1"
-                                    Html.td [
-                                        prop.style [style.maxWidth (length.perc 60)]
-                                        prop.children [
-                                            Daisy.collapse [
-                                                prop.tabIndex 0
-                                                collapse.arrow
-                                                prop.children [
-                                                    Html.input [prop.type' "checkbox"]
-                                                    Daisy.collapseTitle [ 
-                                                        prop.style [
-                                                            // style.width 900
-                                                            style.fontSize 15 
-                                                        ]  
-                                                        prop.text "Example: Reduced Dormancy5 encodes a protein phosphatase 2C that is required for seed dormancy in Arabidopsis"
-                                                    ]
-                                                    Daisy.collapseContent [
-                                                        Daisy.cardBody [
-                                                            prop.style [
-                                                                style.flexDirection.row
-                                                                style.flexWrap.wrap
-                                                                // style.width 900
-                                                                style.fontSize 15
-                                                            ]    
-                                                            prop.children [
-                                                                for word in (splitTextIntoWords exAbstract) do
-                                                                    Html.span [
-                                                                        //prop.className 
-                                                                        prop.onClick (fun _ ->())
-                                                                        prop.text word
-                                                                        prop.className "hover:bg-sky-700"  
-                                                                        prop.style [style.cursor.pointer; style.userSelect.none] 
-                                                                    ]
-                                                            ]
-                                                        ]
-                                                    ]
-                                                ]
-                                            ]
-                                        ]
-                                    ]                    
-                                    Html.td [
-                                        Daisy.collapse [
-                                            prop.tabIndex 0
-                                            collapse.arrow
-                                            prop.children [
-                                                Html.input [prop.type' "checkbox"]
-                                                Daisy.collapseTitle [ 
-                                                    prop.style [
-                                                        style.fontSize 15
-                                                    ]  
-                                                    prop.text "Interactions"
-                                                ]
-                                                Daisy.collapseContent [
-                                                    Daisy.cardBody [
-                                                        prop.style [
-                                                            // style.width 135
-                                                            style.fontSize 15
-                                                            style.color.black
-                                                            style.padding 1
-
-                                                        ]
-                                                        prop.children [ 
-                                                            Daisy.formControl [
-                                                                Daisy.label [Daisy.labelText "Partner 1"]
-                                                                Daisy.input [input.bordered; input.sm]
-                                                            ]
-                                                            Daisy.formControl [
-                                                                Daisy.label [Daisy.labelText "Partner 2"]
-                                                                Daisy.input [input.bordered; input.sm]
-                                                            ]
-                                                            Daisy.formControl [
-                                                                Daisy.label [Daisy.labelText "InteractionType"]
-                                                                Daisy.dropdown [
-                                                                    Daisy.button.button [
-                                                                        button.sm
-                                                                        prop.style [
-                                                                            style.width 135
-                                                                            style.fontSize 15  
-                                                                        ]                                            
-                                                                        prop.text myInput
-                                                                        prop.className "tableElement"
-                                                                    ]
-                                                                    Daisy.dropdownContent [
-                                                                        prop.className "p-1 shadow menu sm-base-150"
-                                                                        prop.style [
-                                                                            style.width 135
-                                                                            style.fontSize 15
-                                                                            style.color.white
-                                                                        ]
-                                                                        prop.tabIndex 0
-                                                                        prop.children [
-                                                                            Html.li [Html.a [prop.text "Protein-Gene"; prop.onClick (fun _ -> setinput "Protein-Gene"); prop.style [style.backgroundColor.aqua]]]
-                                                                            Html.li [Html.a [prop.text "Protein-Protein"; prop.onClick (fun _ -> setinput "Protein-Protein"); prop.style [style.backgroundColor.aqua]]]
-                                                                            Html.li [Html.a [prop.text "Other"; prop.onClick (fun _ -> setinput "Other"); prop.style [style.backgroundColor.aqua]]]
-                                                                        ]
-                                                                    ]
-                                                                ]
-                                                            ]
-                                                        ]
-                                                    ]
-                                                ]
-                                            ]
-                                        ]
-                                    ] 
+                                    Helper.tableCellPaperContent exAbstract 
+                                    Helper.tableCellFormInput(myInput,setinput)
+                                ]
+                            ]
+                            Html.tr [
+                                prop.children [
+                                    Html.td "2"
+                                    Helper.tableCellPaperContent exAbstract 
+                                    Helper.tableCellFormInput(myInput,setinput)
                                 ]
                             ]
                         ]
@@ -205,6 +222,8 @@ type GTtable =
                 ]
             ]
         ]
+
+//Todo 
                    
                 
         
